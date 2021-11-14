fb809e8492e8b3c8e8ab084ccccf2db33779c673
Anca Puscasu <ancapuscasu@ymail.com>
2021-11-14 16:41:48 -0500

    created MainView component and exported it

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class MainView extends React.Component {
+  render() {
+    return (
+      <div className="main-view">
+        <div>Movie1</div>
+        <div>Movie2</div>
+        <div>Movie3</div>
+      </div>
+    );
+  }
+}
