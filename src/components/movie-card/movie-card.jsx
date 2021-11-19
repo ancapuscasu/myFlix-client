f7f456fa914358cba62d9dada4399d8b87fac0bb
Anca Puscasu <ancapuscasu@ymail.com>
2021-11-18 21:55:09 -0500

    updated prop-types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -9,18 +9,14 @@ export class MovieCard extends React.Component {
     }
 }
 
-MovieView.propTypes = {
+MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string.isRequired,
         Description: PropTypes.string.isRequired,
-        Genre: PropTypes.exact({
-            Title: PropTypes.string.isRequired,
-            Description: PropTypes.string.isRequired,
-        }).isRequired,
+        Genre: PropTypes.array.isRequired,
         Director: PropTypes.shape({
             Name: PropTypes.string.isRequired,
-            Bio: PropTypes.string.isRequired,
-            Birthdate: PropTypes.instanceOf(Date).isRequired
+            Bio: PropTypes.string.isRequired
         }).isRequired,
         ImagePath: PropTypes.string.isRequired,
         Featured: PropTypes.bool,
