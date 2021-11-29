0ae2a97980ab30b4c709a700bfe9190cc4e19cf0
Anca Puscasu <ancapuscasu@ymail.com>
2021-11-29 17:34:44 -0500

    Added React-Bootstrap Card styling

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -2,29 +2,20 @@ import React from 'react';
 import { Card, Button, Row, Col } from 'react-bootstrap';
 
 
-const GenreView = ({ genre, onBackClick }) => {
-
-      console.log(props);
-  
-      return (
-        <>
-            <Row>
-                <Col>
-                    <Card>
-                      <Card.Body>
-                      <div className="genre-view">
-                         <span className="label">Name: </span>
-                         <span className="value">{genre.Name}</span>
-                      </div>
-                      <div className="genre-description">
-                          <span className="label">Description: </span>
-                          <span className="value">{genre.Description}</span>
-                      </div>
-                        <Button variant="primary" onClick={() => { onBackClick(null); }}>Back</Button>
-                      </Card.Body>
-                    </Card>
-              </Col>
-           </Row>
-        </>
-      );
-    }
+export const GenreView = ({genre, onBackClick}) => {
+  return (
+    <>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>{genre.name}</Card.Title>
+              <Card.Text>{genre.Description}</Card.Text>
+              <Button variant="primary" onClick={() => { onBackClick(null); }}>Back</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
