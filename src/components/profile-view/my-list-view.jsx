95e6c135ffb0eb291464b9484e4c38cf965c8b1e
Anca Puscasu <ancapuscasu@ymail.com>
2022-01-06 19:33:46 -0700

    updated favourites view

--- src/components/profile-view/my-list-view.jsx
+++ src/components/profile-view/my-list-view.jsx
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
 import { Card, Form, Button, Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
-export function MyListView (props) {
+export function FavouritesListView (props) {
   const movies=props.movies;
-  const favouriteMovies=props.user.FavouriteMovies;
+  const user = props.user;
+
+  const favouriteMovies = movies.filter(movie => user.FavouriteMovies.indexOf(movie._id) > -1);
+  console.log(favouriteMovies);
 
   
-
-
-
- 
+  return (
+      <Container>
+        <Row>
+          <h3 className="p-3">My List</h3>
+        </Row>
+        <Row>
+            {favouriteMovies.map(movie => (
+              <Col xs={12} sm={6} md={3} key={movie._id}>
+                <Card> 
+                  <Card.Img src={movie.ImagePath} crossOrigin="*"/>
+                  <Card.Body>
+                    <Button 
+                      variant="primary" 
+                      as={Link} to={`/movies/${movie.Title}`}> Open
+                      </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              
+            ))}
     
-    return (
-      <h1>hello</h1>
-    );
-    }
+        </Row>
+      </Container>
+  );
+}
