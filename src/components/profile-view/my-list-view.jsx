89a2c8df08d325fad43d4b549b4bd510975ec969
Anca Puscasu <ancapuscasu@ymail.com>
2022-01-11 11:10:51 -0700

    added styling to favourites-list-view

--- src/components/profile-view/my-list-view.jsx
+++ src/components/profile-view/my-list-view.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Card, Form, Button, Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import "./my-list-view.scss";
 
 
 export function FavouritesListView (props) {
@@ -13,27 +14,31 @@ export function FavouritesListView (props) {
 
   
   return (
-      <Container>
-        <Row>
-          <h3 className="p-3">My List</h3>
+      <div className='my-list-view'>
+        <Row className='ml-5'>
+          <h3 className="p-3 favourites-list-title">My List</h3>
         </Row>
-        <Row>
+        <Row className='ml-5 mr-5 my-list-view-container'>
             {favouriteMovies.map(movie => (
-              <Col xs={12} sm={6} md={3} key={movie._id}>
-                <Card> 
-                  <Card.Img src={movie.ImagePath} crossOrigin="*"/>
-                  <Card.Body>
-                    <Button 
-                      variant="primary" 
-                      as={Link} to={`/movies/${movie.Title}`}> Open
-                      </Button>
-                  </Card.Body>
+              <Col xs={12} sm={6} md={4} lg={3} key={movie._id} className="justify-content-center p-3">
+                <Card className='movie-cards p-2'> 
+                  <Link to={`/movies/${movie.Title}`}>
+                      <Card.Img  variant='top' src= { movie.ImagePath } crossOrigin = '*'/>
+                  </Link>
+                  <Card.Body className="p-0">
+                    <Row className="movie-cards-title-group m-2">
+                        <Card.Title className="movie-cards-title"> { movie.Title } </Card.Title>
+                        <Link to={`/movies/${movie.Title}`}>
+                            <Button className="movie-cards-view-movie" variant='link'>View</Button>
+                        </Link>
+                    </Row>
+                </Card.Body>
                 </Card>
               </Col>
               
             ))}
     
         </Row>
-      </Container>
+      </div>
   );
 }
