9fb11b759bdb62237b0aeaab8132076be936a148
Anca Puscasu <ancapuscasu@ymail.com>
2022-01-11 11:19:01 -0700

    added styling to update-profile-view

--- src/components/profile-view/update-profile-view.jsx
+++ src/components/profile-view/update-profile-view.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
-import "./profile-view.scss";
+import "./update-profile-view.scss";
 
 import { Button, Card, Container, Row, Col } from "react-bootstrap";
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -23,22 +23,35 @@ export function UpdateProfileView (props) {
     LastName: (user.LastName), 
     Username: (user.Username), 
     Email: (user.Email), 
-    Password: (user.Password), 
+    Password: '', 
     Birthdate: (user.Birthdate),
   }
 
   const validationSchema = Yup.object({
-    FirstName: Yup.string(),
-    LastName: Yup.string(),
+    FirstName: Yup.string()
+    .required('Please enter your first name.'),
+    LastName: Yup.string()
+    .required('Please enter your last name.'),
     Email: Yup.string()
+      .required('Please enter your email address.')
       .email('Must be a valid e-mail')
       .max(255),
     Username: Yup.string()
-      .min(5, 'Username must be between 5 and 10 characters.')
-      .max(15,'Username must be between 5 and 10 characters.'),
-    // ConfirmPassword: Yup.string()
-    //   .oneOf([Yup.ref('Password'), null], 'Passwords must match')
-    //   .required("Required"),
+      .required('Please enter a username.')
+      .min(5, 'Username must be between 5 and 15 characters.')
+      .max(15,'Username must be between 5 and 15 characters.'),
+    Password: Yup.string()
+      .required('Please enter a password.')
+      .min(8, 'Your password must contain between 8 and 50 characters.')
+      .max(50, 'Your password must contain between 8 and 50 characters.'),
+    ConfirmPassword: Yup.string()
+      .when("Password", {
+        is: val => (val && val.length > 0 ? true : false),
+      then: Yup.string()
+        .oneOf(
+          [Yup.ref("Password")], "Your passwords must match."
+        )
+      }),
   });
 
   const onSubmit = values => {
@@ -89,16 +102,12 @@ export function UpdateProfileView (props) {
 
   console.log("initial value:", initialValues);
   return (
-    <Container className="profile-view">
-      <Row className="p-3 justify-content-around">
-        <h3> Hello {user.FirstName}</h3>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col xs={10} sm={9} md={8} lg={6}>
-          <Card className="update-profile">
+      <Row className="update-profile">
+        <Col xs={12} sm={12} md={10} lg={8} xl={6} className="m-3">
+          <Card className="update-profile-card">
             <Card.Body>
-              <Card.Title className="mb-3">Update Profile</Card.Title>
+              <Card.Title className="mb-3 update-profile-card-title">Hello {user.FirstName}</Card.Title>
+              <Card.Subtitle className="mb-5 text-muted">Update your profile</Card.Subtitle>
               <Formik
                 enableReinitialize
                 initialValues={initialValues}
@@ -106,66 +115,118 @@ export function UpdateProfileView (props) {
                 onSubmit={onSubmit}
               >
                 <Form>
-                  <Row className="mb-3">
-                    <label htmlFor="FirstName">First Name</label>
+
+                  <Row className="mb-2 update-profile-input-group horizontal-group">
+                    <Col className="horizontal-input left">
+                      <Field 
+                        className="update-profile-input"
+                        name="FirstName"
+                        type="text" 
+                        required
+                        // placeholder="First name"
+                      />
+                      <label htmlFor="FirstName" className="update-profile-input-label">First Name</label>
+                      <ErrorMessage 
+                        name="FirstName"
+                        component="div"
+                        className="input-error"
+                      />
+                    </Col>
+
+                    <Col className="horizontal-input right">
+                      <Field
+                        className="update-profile-input"
+                        name="LastName"
+                        type="text" 
+                        required
+                        // placeholder="Last name"
+                      />
+                      <label htmlFor="LastName" className="update-profile-input-label">Last name</label>
+                      <ErrorMessage 
+                        name="LastName"
+                        component="div"
+                        className="input-error"
+                      />
+                    </Col>
+                  </Row>
+
+                  <Row className="mb-2 update-profile-input-group">
                     <Field 
-                      name="FirstName"
-                      type="text" 
-                      placeholder="First name"
-                    />
-                    <ErrorMessage 
-                      name="FirstName"
-                      component="div"
-                      className="text-danger"
-                    />
-                  </Row>
-
-                  <Row className="mb-3">
-                    <label htmlFor="LastName">Last name</label>
-                    <Field
-                      name="LastName"
-                      type="text" 
-                      placeholder="Last name"
-                    />
-                    <ErrorMessage 
-                      name="LastName"
-                      component="div"
-                      className="text-danger"
-                    />
-                  </Row>
-
-                  <Row className="mb-3">
-                    <label htmlFor="Username">Username</label>
-                    <Field  
+                      className="update-profile-input"
                       name="Username"
                       type="text" 
-                      placeholder="Username"
+                      required
+                      // placeholder="Username"
                     />
+                    <label htmlFor="Username" className="update-profile-input-label">Username</label>
                     <ErrorMessage
                       name="Username"
                       component="div"
-                      className="text-danger"
+                      className="input-error"
                     />
                   </Row>
 
-                  <Row className="mb-3">
-                    <label htmlFor="Email">Email</label>
+                  <Row className="update-profile-input-group  horizontal-group mb-2">
+                    <Col className='horizontal-input left'>
+                      <Field  
+                        className="update-profile-input"
+                        name="Password" 
+                        type="password" 
+                        required
+                        // placeholder="Password"
+                      />
+                      <label className="update-profile-input-label" htmlFor="Password">Password</label>
+                      <ErrorMessage 
+                        name="Password"
+                        component="div"
+                        className="input-error"                   
+                      />
+                    </Col>
+
+                    <Col className='horizontal-input right'>
+                      <Field  
+                        className="update-profile-input"
+                        name="ConfirmPassword" 
+                        type="password" 
+                        required
+                        // placeholder="Confirm Password"
+                      />
+                      <label className="update-profile-input-label" htmlFor="ConfirmPassword">Confirm Password</label>
+                      <ErrorMessage 
+                        name="ConfirmPassword"
+                        component="div"
+                        className="input-error"                        
+                      />
+                    </Col>
+                  </Row>
+
+                  <Row className="mb-2 update-profile-input-group">
                     <Field
+                      className="update-profile-input"
                       name="Email" 
-                      type="email" 
-                      placeholder="Email address"
+                      type="text" 
+                      required
+                      // placeholder="Email address"
                     />
+                    <label htmlFor="Email" className="update-profile-input-label">Email</label>
                     <ErrorMessage 
                       name="Email"
                       component="div"
-                      className="text-danger"
+                      className="input-error"
                     />
                   </Row>
 
-                  <Row className="justify-content-around">
-                    <Button variant="outline-warning" type="submit">Update User</Button>
-                    <Button variant="outline-danger" type="submit" onClick={handleDeleteAccount}>Delete Account</Button>
+                  <Row>
+                    <button className="update-button" type="submit">Update User</button>
                   </Row>
+
+                  <Row className="justify-content-end">
+                    <p className='delete-account'>Click here to
+                      <a href='/' type="submit" onClick={handleDeleteAccount} className='ml-1 delete-account-button'>delete account</a>
+                    </p>
+                  </Row>
+
+                  {/* <Button variant="outline-danger" type="submit" onClick={handleDeleteAccount}>Delete Account</Button> */}
 
                 </Form>
               </Formik>
@@ -175,8 +236,6 @@ export function UpdateProfileView (props) {
         <Col></Col>
       </Row>
 
-
-    </Container>
   );
 }
 
